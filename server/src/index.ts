5fd7e80059ce2d1946e1e643f72c90f130e54aa3
Johan Lescure <johan.lescure@gmail.com>
2024-11-22 18:15:31 +0100

    10

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Serve static files from the React build folder
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "..", "..", "client", "build")));
 
 // API routes
 app.get("/api", (req, res) => {
@@ -18,7 +18,9 @@ app.get("/favicon.ico", (req, res) => {
 
 // Handle all other routes by sending the React app's index.html
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(
+    path.join(__dirname, "..", "..", "client", "build", "index.html")
+  );
 });
 
 app.listen(port, () => {
