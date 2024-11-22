d1dca057de1c938d6765c7c30d11f1d5989147ec
Johan Lescure <johan.lescure@gmail.com>
2024-11-22 15:47:05 +0100

    fourth try

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,23 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
+// Serve static files from the React build folder
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+// API routes
+app.get("/api", (req, res) => {
   res.send("Hello from server!");
+});
+
+// Favicon route, if you're using one
+//app.get("/favicon.ico", (req, res) => res.status(204));
+
+// Handle all other routes by sending the React app's index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 app.listen(port, () => {
